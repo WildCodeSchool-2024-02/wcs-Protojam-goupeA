362dafca69d36091ad79125010e1327909907144
pierre delarocque <delarocque.pierre@gmail.com>
2024-05-16 15:30:57 +0200

    modif h1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ import "./App.scss";
 function App() {
   return (
     <>
+      <header className="header-container">
+        <h1 className="titre-h1">Échappée Célébrement Fantasque</h1>
+      </header>
       <nav>
         <NavLink to="/">Acceuil</NavLink>
         <NavLink to="/sejour">Nos échappées</NavLink>
