b68ba28b70e4c993e04396fb20bfdf0a4ab72023
Sachdarras <sachdarras@gmail.com>
2024-05-16 21:35:47 +0200

    fixscss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { NavLink, Outlet } from "react-router-dom";
 import "./App.scss";
 import Footer from "./components/Footer";
+import logo from "./assets/images/logo.png";
 
 function App() {
   return (
     <>
       <header className="header-container">
+        <img className="logo" src={logo} alt="logo" />
         <h1 className="titre-h1">Échappée Célébrement Fantasque</h1>
+        <div>
+          <h6>none</h6>
+        </div>
       </header>
       <nav>
         <NavLink to="/">Acceuil</NavLink>
