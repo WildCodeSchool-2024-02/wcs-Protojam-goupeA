59f0619c349d35b7927f5707f8c9bc39aac5efa1
pierre delarocque <delarocque.pierre@gmail.com>
2024-05-16 21:53:16 +0200

    first commit

--- client/src/pages/Sejour.jsx
+++ client/src/pages/Sejour.jsx
@@ -1,13 +1,40 @@
+import { useState } from "react";
+import categories from "../data/categoriesData";
+
 function Sejour() {
+  const [filterChoice, setFilterChoice] = useState("");
+  console.log(filterChoice);
   return (
     <section>
-      <h1>Hello Toto2</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-        esse blanditiis doloribus repellat harum nesciunt rerum nobis non minus
-        similique earum incidunt alias facilis nam error eaque, repudiandae,
-        odit quis!
-      </p>
+      <h1>Nos Échappée</h1>
+      <select onChange={(e) => setFilterChoice(e.target.value)}>
+        <option value="">All</option>
+        {categories.map((categ) => (
+          <option key={categ.id}>{categ.name}</option>
+        ))}
+      </select>
+      <div className="category-container">
+        {categories
+          .filter(
+            (categorie) => categorie.name === filterChoice || !filterChoice
+          )
+          .map((categorie) => (
+            <div key={categorie.id}>
+              <div className="sousCategorie-container">
+                {categorie.sousCategorie.map((sousCat, index) => (
+                  <div className="echappe-container" key={index}>
+                    <h3>{sousCat.name}</h3>
+                    <img
+                      className="img-container"
+                      src={sousCat.img}
+                      alt={sousCat.name}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+      </div>
     </section>
   );
 }
