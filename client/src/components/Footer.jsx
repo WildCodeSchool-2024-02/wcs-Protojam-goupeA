3119268db5d3230cfe99a9c2822e4baab136a574
Fabien LAQUERRIERE <fabienlqr@gmail.com>
2024-05-16 15:53:10 +0200

    create teamData and use it in Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,18 @@
+import teamTab from "../data/teamData";
+
 function Footer() {
   return (
-    <>
-      <p>plop</p>
-      <p>plop</p>
-    </>
+    <footer>
+      {teamTab.map((person) => (
+        <section key={person.name} className={person.firstName}>
+          <ul>
+            <li>{person.name}</li>
+            <li>{person.firstName}</li>
+            <li>{person.gitHub}</li>
+          </ul>
+        </section>
+      ))}
+    </footer>
   );
 }
 
---
+++ client/src/data/teamData.js
@@ -0,0 +1,34 @@
+const teamTab = [
+  {
+    name: "Laquerriere",
+    firstName: "Fabien",
+    gitHub: "https://github.com/FabienLqr",
+    Avatar: "",
+  },
+  {
+    name: "Darras",
+    firstName: "Sacha",
+    gitHub: "https://github.com/Sachdarras",
+    Avatar: "",
+  },
+  {
+    name: "Delarocque",
+    firstName: "Pierre",
+    gitHub: "https://github.com/PierreDelarocque",
+    Avatar: "",
+  },
+  {
+    name: "Wenck",
+    firstName: "Alexandre",
+    gitHub: "https://github.com/Halcomberg",
+    Avatar: "",
+  },
+  {
+    name: "Helbert",
+    firstName: "Matthieu",
+    gitHub: "https://github.com/MatthWCS",
+    Avatar: "",
+  },
+];
+
+export default teamTab;
