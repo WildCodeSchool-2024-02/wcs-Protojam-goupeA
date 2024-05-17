b8efdd2a9b89fa50927a09ad9b754083df407d0e
MatthWCS <matthieuhelbert67@gmail.com>
2024-05-17 02:42:08 +0200

    third try

--- server/src/Model/ContactManager.php
+++ server/src/Model/ContactManager.php
@@ -11,9 +11,9 @@ class ContactManager extends AbstractManager
     public function insert(array $datacontact)
     {
         $statement = $this->pdo->prepare("INSERT INTO " . static::TABLE .  "
-        (firstname, lastname, email, message) VALUES (:firstname, :lastname, :email, :message)");
-        $statement->bindValue('firstname', $datacontact['firstname'], PDO::PARAM_STR);
+        (lastname, firstname, email, message) VALUES (:lastname, :firstname, :email, :message)");
         $statement->bindValue('lastname', $datacontact['lastname'], PDO::PARAM_STR);
+        $statement->bindValue('firstname', $datacontact['firstname'], PDO::PARAM_STR);
         $statement->bindValue('email', $datacontact['email'], PDO::PARAM_STR);
         $statement->bindValue('message', $datacontact['message'], PDO::PARAM_STR);
         $statement->execute();
